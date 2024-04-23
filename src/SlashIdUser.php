9f0ecb0bc0e78ad718dafdd83c3c56c7d02b37d5
José San Martin <jose@josesanmartin.net>
2024-04-23 17:29:28 -0300

    Standardizes roles

--- src/SlashIdUser.php
+++ src/SlashIdUser.php
@@ -9,7 +9,13 @@ class SlashIdUser extends Person implements UserInterface
 {
     public function getRoles(): array
     {
-        return array_merge(['ROLE_USER'], $this->getGroups());
+        return array_merge(
+            ['ROLE_USER'],
+            array_map(
+                fn(string $group) => 'ROLE_' . strtoupper($group),
+                $this->getGroups(),
+            ),
+        );
     }
 
     public function eraseCredentials(): void {}
