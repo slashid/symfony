dad0bf468f0513dac6e2578387a7ba19bec0c869
José San Martin <jose@josesanmartin.net>
2024-04-27 13:27:08 -0300

    Script to generate the translation Yaml

---
+++ src/Development/TranslationYamlGenerator.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SlashId\Symfony\Development;
+
+use Symfony\Component\Yaml\Yaml;
+
+/**
+ * Script to generate `translations/SlashIdSymfonyBundle.en.yml` from React JSON, to be used for development only.
+ */
+final class TranslationYamlGenerator
+{
+    const SOURCE_URL = 'https://raw.githubusercontent.com/slashid/javascript/main/packages/react/src/components/text/constants.ts';
+
+    public static function generate(): void
+    {
+        self::assertDevelopmentEnvironment();
+
+        // Downloads file and converts TypeScript into JSON.
+        $source = file_get_contents(self::SOURCE_URL);
+        $jsonStartPosition = strpos($source, '{');
+        $jsonEndPosition = strpos($source, '};');
+        $source = substr($source, $jsonStartPosition + 1, $jsonEndPosition - $jsonStartPosition - 1);
+        $sourceLines = explode("\n", $source);
+        $sourceLines = array_values(array_filter($sourceLines, fn($line) => strpos(trim($line), '"') === 0));
+
+        // Remove comma in the last line, to make JSON valid.
+        $lastLine = array_pop($sourceLines);
+        $lastLine = trim($lastLine, "\n\r\t\v\0,");
+        $sourceLines[] = $lastLine;
+
+        // Reconstructs the JSON and makes it into a string.
+        $validJson = '{' . implode("\n", $sourceLines) . '}';
+        $strings = json_decode($validJson, TRUE);
+
+        // Removes invalid string.
+        unset($strings['']);
+
+        // Customize strings for Symfony.
+        $strings = [
+            'initial.title' => 'Welcome',
+            'success.title' => 'Completing authentication...',
+            'success.subtitle' => "You'll be logged in in a few moments",
+        ] + $strings;
+
+        // Writes Yaml.
+        file_put_contents(__DIR__ . '/../../translations/SlashIdSymfonyBundle.en.yml', Yaml::dump($strings, 100));
+    }
+
+    /**
+     * Prevents running this script inside a package.
+     */
+    private static function assertDevelopmentEnvironment(): void
+    {
+        if (!file_exists(__DIR__ . '/../../vendor/autoload.php')) {
+            throw new \Exception('You must run this script only on a standalone repository.');
+        }
+    }
+}
