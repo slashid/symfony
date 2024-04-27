816ab8c1aaabf5ad3e2db8b6af64c8f12f167098
José San Martin <jose@josesanmartin.net>
2024-04-27 13:35:06 -0300

    Stores translations in config cache

--- config/definition.php
+++ config/definition.php
@@ -1,8 +1,18 @@
 <?php
 
 use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\Translation\Loader\ArrayLoader;
+use Symfony\Component\Yaml\Yaml;
 
 return static function (DefinitionConfigurator $definition) {
+    // Lists the strings IF the package symfony/translation exists.
+    $translationStrings = NULL;
+    if (class_exists(ArrayLoader::class)) {
+        $stringsFromYaml = Yaml::parse(file_get_contents(__DIR__ . '/../translations/SlashIdSymfonyBundle.en.yml'));
+        $catalogue = (new ArrayLoader())->load($stringsFromYaml, 'en', 'SlashIdSymfonyBundle');
+        $translationStrings = array_keys($catalogue->all('SlashIdSymfonyBundle'));
+    }
+
     $definition->rootNode()->children()
         ->scalarNode('environment')
             ->defaultValue('%env(trim:string:SLASHID_ENVIRONMENT)%')
@@ -14,6 +24,10 @@ return static function (DefinitionConfigurator $definition) {
             ->end()
         ->scalarNode('api_key')
             ->defaultValue('%env(trim:string:SLASHID_API_KEY)%')
+            ->cannotBeOverwritten()
+            ->end()
+        ->variableNode('translation_strings')
+            ->defaultValue($translationStrings)
             ->cannotBeOverwritten()
             ->end()
 
